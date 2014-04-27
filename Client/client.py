f19c27367cca6370ecea7264923ec6e60f86ed73
Marbo <mc2cy@virginia.edu>
2014-04-27 18:00:00 -0400

    userstat now incorporate into admin, admin can now see all users in server

--- Client/client.py
+++ Client/client.py
@@ -15,8 +15,8 @@ import pickle
 
 
 
-HOST = 'http://127.0.0.1:5000/'
-
+#HOST = 'http://172.25.208.188:5000/'
+HOST = 'http://0.0.0.0:5000/'
 WORKING_DIR = ""
 SUID = ""
 PROC_QUEUE = Queue()
@@ -137,6 +137,7 @@ def server_sync():
                 for path in paths:
                     if ONCE:
                         path = path.replace(ROOT, WORKING_DIR)
+                        print path
                         if os.path.exists(path) == False:
                             # print path
                             logging.debug("Checking if " + path + " is a directory...")
@@ -351,19 +352,21 @@ def admin_change_pass():
         print "ERROR: Username not found! Please try again!"
 
 def user_stat():
-    print "To see stats of user, please input"
-    username = raw_input('Username: ')
-    r = requests.get(HOST+"stat/"+username)
+    #print "To see stats of user, please input"
+    #username = raw_input('Username: ')
+    r = requests.get(HOST+"userstat")
     result = yaml.load(r.text)
 
     if result[0] == "400":
-        logging.debug("Unsucessful retrieval of " + username + " stats")
-        print "Unsuccesful retrieval of " + username + " stats"
+        logging.debug("Unsucessful retrieval of  users stats")
+        print "Unsuccesful retrieval of user stats"
     elif result[0] == "200":
+        logging.debug("Succesfully retrieved users stats")
+        print "Succesfully retrieved users stats"
         print result[1]
     else:
-        logging.debug("Succesfully retrieved " + username + " stats")
-        print "Succesfully retrieved " + username + " stats"
+        logging.debug("Succesfully retrieved users stats")
+        print "Succesfully retrieved users stats"
 
 def synchronize():
     global SYNCRHONIZED
